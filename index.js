648a7f9975e2be12bab0b6685d7abf9a70425f90
Zachary Berardo <zachary@zachnthebox.com>
2018-05-13 16:19:23 -0400

    feat: add random image ability

--- index.js
+++ index.js
@@ -1,23 +1,66 @@
 const express = require('express');
 const sharp = require('sharp');
+const fs = require('fs');
+const util = require('util');
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.get('/image/:width/:height', (req, res) => {
+function getRequestedImageSize(req) {
   const width = parseInt(req.params.width, 10) || 300;
   const height = parseInt(req.params.height, 10) || 300;
+  return {
+    width,
+    height
+  };
+}
 
-  let image = 'images/1.jpg';
+function getRandomImageFileName() {
+  const i = Math.floor(Math.random() * 11) + 1;
+  return `images/${i}.jpg`;
+}
 
-  sharp(image).resize(width, height).toBuffer().then(buffer => {
-    res.header('Content-Disposition', 'inline; filename="zach.jpg"');
+function checkSizes(width, height) {
+  if (parseFloat(width) < 1 || parseFloat(height) < 1) {
+    throw 'Bad Request';
+  } else if (width > 3500 || height > 3500) {
+    throw 'Too Large';
+  }
+}
+
+async function saveImage(width, height, buffer) {
+  await util.promisify(fs.writeFile)(`/tmp/${width}x${height}.jpg`, buffer);
+}
+
+function getImage(width, height) {
+  const path = `/tmp/${width}x${height}.jpg`;
+  return fs.existsSync(path) && path || null;
+}
+
+app.get('/image/:width/:height', (req, res) => {
+  const {
+    width,
+    height
+  } = getRequestedImageSize(req);
+
+  checkSizes(width, height);
+
+
+  let filename = getImage(width, height);
+  if (filename) {
+    res.header('Cache-Control', 'public, max-age=31536000');
+    res.sendFile(filename);
+    return;
+  }
+  filename = getRandomImageFileName();
+
+  sharp(filename).resize(width, height).toBuffer().then(buffer => {
+    res.header('Content-Disposition', `inline; filename="${width}x${height}.jpg"`);
     res.header('Cache-Control', 'public, max-age=31536000');
     res.header('Content-Type', 'image/jpeg');
+    saveImage(width, height, buffer);
     res.send(buffer);
-  }).catch(() => {
-    res.status(500);
   });
 });
 
