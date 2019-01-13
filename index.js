ceb98e747f924a3710507b5a9781b15b5506d03a
Zachary Berardo <zachary@zachnthebox.com>
2019-01-13 09:32:47 -0500

    fix: work with now v2;

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const sharp = require('sharp');
+const Jimp = require('jimp');
 const fs = require('fs');
 const util = require('util');
 
@@ -18,7 +18,7 @@ function getRequestedImageSize(req) {
 
 function getRandomImageFileName() {
   const i = Math.floor(Math.random() * 11) + 1;
-  return `images/${i}.jpg`;
+  return `${__dirname}/images/${i}.jpg`;
 }
 
 function checkSizes(width, height) {
@@ -38,8 +38,6 @@ function getImage(width, height) {
   return fs.existsSync(path) && path || null;
 }
 
-app.use(express.static(__dirname + '/public'));
-
 app.get('/image/:width/:height', (req, res) => {
   const {
     width,
@@ -57,12 +55,18 @@ app.get('/image/:width/:height', (req, res) => {
   }
   filename = getRandomImageFileName();
 
-  sharp(filename).resize(width, height).toBuffer().then(buffer => {
+  Jimp.read(filename).then(image => {
+    return image.resize(width, height).getBufferAsync(Jimp.MIME_JPEG);
+  }).then(buffer => {
     res.header('Content-Disposition', `inline; filename="${width}x${height}.jpg"`);
     res.header('Cache-Control', 'public, max-age=31536000');
     res.header('Content-Type', 'image/jpeg');
     saveImage(width, height, buffer);
     res.send(buffer);
+  }).catch(err => {
+    res.send({
+      err: 'An unknown error occured.',
+    });
   });
 });
 
